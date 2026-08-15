77dabd8c56b8ad2007b24eb972478b4483d79872
Nadr59 <117353272+Nadr59@users.noreply.github.com>
2026-08-15 07:31:53 +0300

    Refactor plugin and dependency management settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,16 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
         gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.google.com")
     }
 }
 
-dependencyResolution {
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
         mavenCentral()
+        maven("https://maven.google.com")
     }
 }
 
