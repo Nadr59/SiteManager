3aa001edcdf6279eaa4d3a09d4aa37bd15238522
Nadr59 <117353272+Nadr59@users.noreply.github.com>
2026-08-31 14:29:02 +0300

    Add Markdown renderer dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,4 +87,7 @@ dependencies {
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+// Markdown renderer
+    implementation("com.github.jeziellago:compose-markdown:0.5.4")
+}
 }
