93a44d1228d3443d55645806327157f7f12e96b7
Nadr59 <117353272+Nadr59@users.noreply.github.com>
2026-09-01 23:10:43 +0300

    Refactor SiteManagerApp by removing Timber setup
    Removed Timber logging initialization from SiteManagerApp.

--- app/src/main/java/com/nadr59/sitemanager/SiteManagerApp.kt
+++ app/src/main/java/com/nadr59/sitemanager/SiteManagerApp.kt
@@ -1,20 +1,8 @@
-// app/src/main/java/com/nadr59/sitemanager/SiteManagerApp.kt
-
 package com.nadr59.sitemanager
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
-class SiteManagerApp : Application() {
+class SiteManagerApp : Application()
 
-    override fun onCreate() {
-        super.onCreate()
-
-        // تهيئة Timber للـ logging
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-    }
-}
