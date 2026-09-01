443189ae03dd78b1444d04dcf9132ac545dece6f
Nadr59 <117353272+Nadr59@users.noreply.github.com>
2026-09-01 22:41:52 +0300

    Delete app/src/main/java/com/nadr59/sitemanager/domain/translator/WebPageTranslaionCoordinatorTest.kt

--- app/src/main/java/com/nadr59/sitemanager/domain/translator/WebPageTranslaionCoordinatorTest.kt
+++
@@ -1,6 +0,0 @@
-// app/src/test/java/com/nadr59/sitemanager/domain/translator/WebPageTranslationCoordinatorTest.kt
-
-package com.nadr59.sitemanager.domain.translator
-
-// TODO: إضافة الاختبارات لاحقاً
-// يتطلب إضافة مكتبات mockk و coroutines-test
