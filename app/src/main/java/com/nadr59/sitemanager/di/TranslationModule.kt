a53bb775a8c7e7d1812769a53549870fc3388b12
Nadr59 <117353272+Nadr59@users.noreply.github.com>
2026-09-03 16:03:48 +0300

    Replace DefaultTranslationEngine with InkTranslationEngine

--- app/src/main/java/com/nadr59/sitemanager/di/TranslationModule.kt
+++ app/src/main/java/com/nadr59/sitemanager/di/TranslationModule.kt
@@ -1,12 +1,9 @@
 package com.nadr59.sitemanager.di
 
-import com.nadr59.sitemanager.data.repository.TranslationRepository
-import com.nadr59.sitemanager.domain.translator.DefaultTranslationEngine
+import com.nadr59.sitemanager.domain.translator.InkTranslationEngine
 import com.nadr59.sitemanager.domain.translator.TranslationEngine
-import com.nadr59.sitemanager.domain.translator.WebPageTranslationCoordinator
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -18,19 +15,6 @@ abstract class TranslationModule {
     @Binds
     @Singleton
     abstract fun bindTranslationEngine(
-        implementation: DefaultTranslationEngine
+        inkTranslationEngine: InkTranslationEngine
     ): TranslationEngine
-}
-
-@Module
-@InstallIn(SingletonComponent::class)
-object TranslationProviderModule {
-
-    @Provides
-    @Singleton
-    fun provideWebPageTranslationCoordinator(
-        repository: TranslationRepository
-    ): WebPageTranslationCoordinator {
-        return WebPageTranslationCoordinator(repository)
-    }
 }
